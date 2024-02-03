a4af7a0a48da9eee3e9f941c31421f9c5abec546
Ramareddy <ramareddy9652@gmail.com>
2024-02-03 16:54:22 +0530

    done changes

--- assets/js/basic-calculator.js
+++ assets/js/basic-calculator.js
@@ -1,40 +1,20 @@
-function calculate(s) {
-  let stack = [];
-  let num = 0;
-  let sign = 1;
-  let result = 0;
+let displayValue = '';
 
-  for (let i = 0; i < s.length; i++) {
-      const char = s.charAt(i);
-
-      if (char >= '0' && char <= '9') {
-          num = num * 10 + parseInt(char);
-      } else if (char === '+') {
-          result += sign * num;
-          num = 0;
-          sign = 1;
-      } else if (char === '-') {
-          result += sign * num;
-          num = 0;
-          sign = -1;
-      } else if (char === '(') {
-          stack.push(result);
-          stack.push(sign);
-          result = 0;
-          sign = 1;
-      } else if (char === ')') {
-          result += sign * num;
-          num = 0;
-
-          result *= stack.pop(); // pop sign
-          result += stack.pop(); // pop result from the corresponding '('
-      }
-  }
-
-  result += sign * num;
-
-  return result;
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById('display').value = displayValue;
 }
 
+function clearDisplay() {
+  displayValue = '';
+  document.getElementById('display').value = displayValue;
+}
 
-console.log(calculate("1 + 1"));
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue).toString();
+    document.getElementById('display').value = displayValue;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
